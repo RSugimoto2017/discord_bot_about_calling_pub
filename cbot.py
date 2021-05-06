f1aca7a9e4b62ef7fe4c2735c4ff7f21a46338d1
RSugimoto2017 <sryosuke2017@gmail.com>
2021-05-06 21:06:28 +0900

    #2 強制退出までをパブリック版に統合

--- cbot.py
+++ cbot.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # インストールした discord.py を読み込む
 import discord
 import random
@@ -9,7 +10,7 @@ TOKEN = '**************************************************'
 # 接続に必要なオブジェクトを生成
 client = discord.Client()
 
-CHANNEL_ID = *************************
+CHANNEL_ID = '*************************'
 
 # 起動時に動作する処理
 @client.event
@@ -23,18 +24,6 @@ async def on_message(message):
     # メッセージ送信者がBotだった場合は無視する
     if message.author.bot:
         return
-    # 「/neko」と発言したら「にゃーん」が返る処理
-    # if message.content == '^neko':
-    #     await message.channel.send('にゃーん')
-    #     #await message.channel.send(message.author.id)
-
-    # # 「/neko」と発言したら「にゃーん」が返る処理
-    # if message.content == '^duel':
-    #     rand = random.randint(0, 1)
-    #     if rand == 0:
-    #         await message.channel.send(f'{message.author.name}は先攻です！')
-    #     if rand == 1:
-    #         await message.channel.send(f'{message.author.name}は後攻です！')
     if message.content == "^join":
         if message.author.voice is None:
             await message.channel.send("ボイスチャンネルに入ってから呼んでね！")
@@ -47,13 +36,15 @@ async def on_message(message):
         if message.guild.voice_client is None:
             await message.channel.send("初めからボイスチャンネルに入ってないよ！")
             return
-
         # 切断する
         await message.guild.voice_client.disconnect()
-
         await message.channel.send("また呼んでね！")
 
-
+    if message.content == "^goodnight":
+        if message.author.voice is not None and message.guild.me in message.author.voice.channel.members:
+            await allkick(message)
+        else:
+            await message.channel.send("botを自分と同じボイスチャンネルに入れてからにしてね！")
         
     #if "^mute" in message.content:
     if message.content == "^mute":
@@ -68,33 +59,35 @@ async def on_message(message):
             await unmute(message)
         else:
             await message.channel.send("botを自分と同じボイスチャンネルに入れてからにしてね！")
-        
-
+            
     #bot停止(管理者のみ)
     if "^stop" in message.content:
-        if message.author.id == *************************:
+        if message.author.id == '*************************':
             await message.channel.send("ばいばーい！")
             await client.logout()
         else:
             await message.channel.send("管理者専用コマンドだよ！")
     #   クレジット表示
     if "^credit" in message.content:
-        authormem = await client.fetch_user(*************************)
+        authormem = await client.fetch_user('*************************')
         await message.channel.send(f"製作者は「{authormem.name}」です。")
     #   ヘルプ表示
     if "^help" in message.content:
-        await message.channel.send("-----------------------------------------------メイン機能-------------------------------------------------\n\
+        await message.channel.send("```fix\n\
+------------------------------メイン機能--------------------------------\n\
 ボイスチャンネルを開始時、テキストチャットに通知が出ます。\n\
 ボイスチャンネルを終了時、テキストチャットに通知が出ます。\n\
 「^join」：botをボイスチャンネルに参加させます。\n\
 「^leave」：botをボイスチャンネルから退出させます。\n\
 「^mute」：自分とbotが参加しているボイスチャンネルの全員をミュートします。\n\
 「^unmute」：自分botが参加しているボイスチャンネルの全員をミュート解除します。\n\
-------------------------------------------------サブ機能--------------------------------------------------\n\
+「^goodnight」：自分とbotが参加しているボイスチャンネルの全員を退出させます。\n\
+--------------------------------サブ機能--------------------------------\n\
 「^stop」：botをオフラインにします。(管理者(現在のところもぎ)のみ)\n\
 「^help」：操作説明を表示します。\n\
 「^credit」：製作者について表示します。\n\
-----------------------------------------------------------------------------------------------------------")
+------------------------------------------------------------------------\n\
+```")
         
 
 @client.event
@@ -114,8 +107,6 @@ async def on_vc_end(member,channel):
 @client.event
 async def on_voice_state_update(member,before,after):
     if before.channel != after.channel:
-        #print(before.channel)
-        #print(after.channel)
         # before.channelとafter.channelが異なるなら入退室
         if after.channel and len(after.channel.members) == 1:
             #print(len(after.channel.members))
@@ -127,7 +118,12 @@ async def on_voice_state_update(member,before,after):
             # もし、ボイスチャットが終了したら
             client.dispatch("vc_end",member,before.channel) #発火！
 
-
+async def allkick(message):
+    if message.author.guild_permissions.administrator:
+        vc = message.guild.me.voice.channel
+        await message.channel.send("おやすみなさい！")
+        for member in vc.members:
+            await member.move_to(None)
 
 async def mute(message):
     if message.author.guild_permissions.administrator:
@@ -149,9 +145,6 @@ async def unmute(message):
             await member.edit(mute=False) # チャンネルの各参加者をミュート解除する
     else:
         await message.channel.send("管理者じゃないよ！")
-        
- 
-
 
 # Botの起動とDiscordサーバーへの接続
 client.run(TOKEN)
